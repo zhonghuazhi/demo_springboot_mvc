d99bc61f4dbd1c866aebc95b2ccedfa1357b7c3d
zhizhonghua <zhizhonghua@boco.com.cn>
2019-03-14 18:00:48 +0800

    版本初始化

---
+++ src/main/java/net/cc/demo_springboot_mvc/DemoSpringbootMvcApplication.java
@@ -0,0 +1,13 @@
+package net.cc.demo_springboot_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/cc/demo_springboot_mvc/DemoSpringbootMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package net.cc.demo_springboot_mvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringbootMvcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
